fae9ac68108e6769cdd227bf12d4864fd7789ad1
Chong Xuan Liang <xuan.lc113@gmail.com>
2020-10-20 16:57:05 +0800

    complete homepage styling

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,26 +1,43 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import "./Home.css"
 
-export default function Home() {
+function Home(props) {
   const [roomId, setRoomId] = useState("");
+  const [error, setError] = useState(false);
+
+  const submitHandler = (event) => {
+    const reg = new RegExp("[a-zA-Z0-9]{4,}");
+    if (!reg.test(roomId)) {
+      event.preventDefault();
+      setError(true);
+    } else {
+      props.history.push(`/${roomId}`);
+    }
+  }
 
   return (
     <div className="home">
       <div className="title">
-        <p>Home</p>
+        <p>Chat</p>
       </div>
-      <div className="goto">
-        <input
-          type="text"
-          value={roomId}
-          onChange={(e) => setRoomId(e.target.value)}
-          placeholder="Create a room"
-        />
-        <button>
-          <Link to={`/${roomId}`}>Enter Room</Link>
-        </button>
-      </div>
+        <form onSubmit={submitHandler} className="to-room-form">
+          <div className="to-room-input-block">
+            <input
+              type="text"
+              value={roomId}
+              onChange={(e) => setRoomId(e.target.value)}
+              placeholder="Create a room"
+              className={"to-room-input" + (error ? " error": "")}
+            />
+            {error && 
+              <p className="to-room-error">Please enter at least 4 alphanumeric characters</p>
+            }
+          </div>
+          <button className="to-room-button">Enter Room</button>
+        </form>
     </div>
   );
 }
+
+export default withRouter(Home);
